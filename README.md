21eaea3a8f3985007c526d6e2228c15db7a0f028
Aleksander Kamieński <aleksanderkamienski.97@wp.pl>
2020-06-30 14:42:24 +0200

    change readme, production bootstrap

--- README.md
+++ README.md
@@ -1,14 +1,6 @@
-# DotNet Forum Starter
+The application was created with the help of Wes Doyle ASP.NET Core 2 MVC Forum Tutorial.
+https://www.youtube.com/c/WesDoyle/
 
-A forum app built with ASP.NET Core 2.0. Uses ASP.NET Core Identity for user membership
+This application is available at:
+https://easytrip.azurewebsites.net/
 
-This app provides a starting point for developing a Forum Application with:
-- Forums, Posts, and Replies
-- User Profiles
-- Home Page with Latest Posts
-- Post Content Search
-- Unit testing with NUnit, FluentAssertions
-- Custom CSS
-
-I created a video series demonstrating the building of this code at [ProductiveDev](https://productivedev.com/product/net-core-2-0-mvc-forum-application-course/).
-# easy-trip-app
---
+++ forum-app-demo/obj/Release/netcoreapp2.0/PubTmp/EFSQLScripts/Forum.Data.ApplicationDbContext.sql
@@ -0,0 +1,362 @@
+﻿IF OBJECT_ID(N'__EFMigrationsHistory') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20200410070422_first')
+BEGIN
+    CREATE TABLE [AspNetRoles] (
+        [Id] nvarchar(450) NOT NULL,
+        [ConcurrencyStamp] nvarchar(max) NULL,
+        [Name] nvarchar(256) NULL,
+        [NormalizedName] nvarchar(256) NULL,
+        CONSTRAINT [PK_AspNetRoles] PRIMARY KEY ([Id])
+    );
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20200410070422_first')
+BEGIN
+    CREATE TABLE [AspNetUsers] (
+        [Id] nvarchar(450) NOT NULL,
+        [AccessFailedCount] int NOT NULL,
+        [ConcurrencyStamp] nvarchar(max) NULL,
+        [Email] nvarchar(256) NULL,
+        [EmailConfirmed] bit NOT NULL,
+        [IsActive] bit NOT NULL,
+        [IsAdmin] bit NOT NULL,
+        [LockoutEnabled] bit NOT NULL,
+        [LockoutEnd] datetimeoffset NULL,
+        [MemberSince] datetime2 NOT NULL,
+        [NormalizedEmail] nvarchar(256) NULL,
+        [NormalizedUserName] nvarchar(256) NULL,
+        [PasswordHash] nvarchar(max) NULL,
+        [PhoneNumber] nvarchar(max) NULL,
+        [PhoneNumberConfirmed] bit NOT NULL,
+        [ProfileImageUrl] nvarchar(max) NULL,
+        [Rating] int NOT NULL,
+        [SecurityStamp] nvarchar(max) NULL,
+        [TwoFactorEnabled] bit NOT NULL,
+        [UserDescription] nvarchar(max) NULL,
+        [UserName] nvarchar(256) NULL,
+        CONSTRAINT [PK_AspNetUsers] PRIMARY KEY ([Id])
+    );
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20200410070422_first')
+BEGIN
+    CREATE TABLE [Groups] (
+        [Id] nvarchar(450) NOT NULL,
+        [AdminId] nvarchar(max) NULL,
+        [Description] nvarchar(max) NULL,
+        [ImageUrl] nvarchar(max) NULL,
+        [Name] nvarchar(max) NULL,
+        CONSTRAINT [PK_Groups] PRIMARY KEY ([Id])
+    );
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20200410070422_first')
+BEGIN
+    CREATE TABLE [AspNetRoleClaims] (
+        [Id] int NOT NULL IDENTITY,
+        [ClaimType] nvarchar(max) NULL,
+        [ClaimValue] nvarchar(max) NULL,
+        [RoleId] nvarchar(450) NOT NULL,
+        CONSTRAINT [PK_AspNetRoleClaims] PRIMARY KEY ([Id]),
+        CONSTRAINT [FK_AspNetRoleClaims_AspNetRoles_RoleId] FOREIGN KEY ([RoleId]) REFERENCES [AspNetRoles] ([Id]) ON DELETE CASCADE
+    );
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20200410070422_first')
+BEGIN
+    CREATE TABLE [AspNetUserClaims] (
+        [Id] int NOT NULL IDENTITY,
+        [ClaimType] nvarchar(max) NULL,
+        [ClaimValue] nvarchar(max) NULL,
+        [UserId] nvarchar(450) NOT NULL,
+        CONSTRAINT [PK_AspNetUserClaims] PRIMARY KEY ([Id]),
+        CONSTRAINT [FK_AspNetUserClaims_AspNetUsers_UserId] FOREIGN KEY ([UserId]) REFERENCES [AspNetUsers] ([Id]) ON DELETE CASCADE
+    );
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20200410070422_first')
+BEGIN
+    CREATE TABLE [AspNetUserLogins] (
+        [LoginProvider] nvarchar(450) NOT NULL,
+        [ProviderKey] nvarchar(450) NOT NULL,
+        [ProviderDisplayName] nvarchar(max) NULL,
+        [UserId] nvarchar(450) NOT NULL,
+        CONSTRAINT [PK_AspNetUserLogins] PRIMARY KEY ([LoginProvider], [ProviderKey]),
+        CONSTRAINT [FK_AspNetUserLogins_AspNetUsers_UserId] FOREIGN KEY ([UserId]) REFERENCES [AspNetUsers] ([Id]) ON DELETE CASCADE
+    );
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20200410070422_first')
+BEGIN
+    CREATE TABLE [AspNetUserRoles] (
+        [UserId] nvarchar(450) NOT NULL,
+        [RoleId] nvarchar(450) NOT NULL,
+        CONSTRAINT [PK_AspNetUserRoles] PRIMARY KEY ([UserId], [RoleId]),
+        CONSTRAINT [FK_AspNetUserRoles_AspNetRoles_RoleId] FOREIGN KEY ([RoleId]) REFERENCES [AspNetRoles] ([Id]) ON DELETE CASCADE,
+        CONSTRAINT [FK_AspNetUserRoles_AspNetUsers_UserId] FOREIGN KEY ([UserId]) REFERENCES [AspNetUsers] ([Id]) ON DELETE CASCADE
+    );
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20200410070422_first')
+BEGIN
+    CREATE TABLE [AspNetUserTokens] (
+        [UserId] nvarchar(450) NOT NULL,
+        [LoginProvider] nvarchar(450) NOT NULL,
+        [Name] nvarchar(450) NOT NULL,
+        [Value] nvarchar(max) NULL,
+        CONSTRAINT [PK_AspNetUserTokens] PRIMARY KEY ([UserId], [LoginProvider], [Name]),
+        CONSTRAINT [FK_AspNetUserTokens_AspNetUsers_UserId] FOREIGN KEY ([UserId]) REFERENCES [AspNetUsers] ([Id]) ON DELETE CASCADE
+    );
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20200410070422_first')
+BEGIN
+    CREATE TABLE [PrivateMessages] (
+        [Id] int NOT NULL IDENTITY,
+        [Created] datetime2 NOT NULL,
+        [Description] nvarchar(max) NULL,
+        [ReceiverId] nvarchar(450) NULL,
+        [SenderId] nvarchar(450) NULL,
+        [Subject] nvarchar(max) NULL,
+        CONSTRAINT [PK_PrivateMessages] PRIMARY KEY ([Id]),
+        CONSTRAINT [FK_PrivateMessages_AspNetUsers_ReceiverId] FOREIGN KEY ([ReceiverId]) REFERENCES [AspNetUsers] ([Id]) ON DELETE NO ACTION,
+        CONSTRAINT [FK_PrivateMessages_AspNetUsers_SenderId] FOREIGN KEY ([SenderId]) REFERENCES [AspNetUsers] ([Id]) ON DELETE NO ACTION
+    );
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20200410070422_first')
+BEGIN
+    CREATE TABLE [ApplicationUserGroup] (
+        [Id] nvarchar(450) NOT NULL,
+        [ApplicationUserId] nvarchar(450) NULL,
+        [GroupId] nvarchar(450) NULL,
+        CONSTRAINT [PK_ApplicationUserGroup] PRIMARY KEY ([Id]),
+        CONSTRAINT [FK_ApplicationUserGroup_AspNetUsers_ApplicationUserId] FOREIGN KEY ([ApplicationUserId]) REFERENCES [AspNetUsers] ([Id]) ON DELETE NO ACTION,
+        CONSTRAINT [FK_ApplicationUserGroup_Groups_GroupId] FOREIGN KEY ([GroupId]) REFERENCES [Groups] ([Id]) ON DELETE NO ACTION
+    );
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20200410070422_first')
+BEGIN
+    CREATE TABLE [Forums] (
+        [Id] int NOT NULL IDENTITY,
+        [Created] datetime2 NOT NULL,
+        [Description] nvarchar(max) NULL,
+        [GroupId] nvarchar(450) NULL,
+        [ImageUrl] nvarchar(max) NULL,
+        [Title] nvarchar(max) NULL,
+        CONSTRAINT [PK_Forums] PRIMARY KEY ([Id]),
+        CONSTRAINT [FK_Forums_Groups_GroupId] FOREIGN KEY ([GroupId]) REFERENCES [Groups] ([Id]) ON DELETE NO ACTION
+    );
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20200410070422_first')
+BEGIN
+    CREATE TABLE [Posts] (
+        [Id] int NOT NULL IDENTITY,
+        [Content] nvarchar(max) NULL,
+        [Created] datetime2 NOT NULL,
+        [ForumId] int NULL,
+        [IsArchived] bit NOT NULL,
+        [Title] nvarchar(max) NULL,
+        [UserId] nvarchar(450) NULL,
+        CONSTRAINT [PK_Posts] PRIMARY KEY ([Id]),
+        CONSTRAINT [FK_Posts_Forums_ForumId] FOREIGN KEY ([ForumId]) REFERENCES [Forums] ([Id]) ON DELETE NO ACTION,
+        CONSTRAINT [FK_Posts_AspNetUsers_UserId] FOREIGN KEY ([UserId]) REFERENCES [AspNetUsers] ([Id]) ON DELETE NO ACTION
+    );
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20200410070422_first')
+BEGIN
+    CREATE TABLE [PostReplies] (
+        [Id] int NOT NULL IDENTITY,
+        [Content] nvarchar(max) NULL,
+        [Created] datetime2 NOT NULL,
+        [PostId] int NULL,
+        [UserId] nvarchar(450) NULL,
+        CONSTRAINT [PK_PostReplies] PRIMARY KEY ([Id]),
+        CONSTRAINT [FK_PostReplies_Posts_PostId] FOREIGN KEY ([PostId]) REFERENCES [Posts] ([Id]) ON DELETE NO ACTION,
+        CONSTRAINT [FK_PostReplies_AspNetUsers_UserId] FOREIGN KEY ([UserId]) REFERENCES [AspNetUsers] ([Id]) ON DELETE NO ACTION
+    );
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20200410070422_first')
+BEGIN
+    CREATE INDEX [IX_ApplicationUserGroup_ApplicationUserId] ON [ApplicationUserGroup] ([ApplicationUserId]);
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20200410070422_first')
+BEGIN
+    CREATE INDEX [IX_ApplicationUserGroup_GroupId] ON [ApplicationUserGroup] ([GroupId]);
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20200410070422_first')
+BEGIN
+    CREATE INDEX [IX_AspNetRoleClaims_RoleId] ON [AspNetRoleClaims] ([RoleId]);
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20200410070422_first')
+BEGIN
+    CREATE UNIQUE INDEX [RoleNameIndex] ON [AspNetRoles] ([NormalizedName]) WHERE [NormalizedName] IS NOT NULL;
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20200410070422_first')
+BEGIN
+    CREATE INDEX [IX_AspNetUserClaims_UserId] ON [AspNetUserClaims] ([UserId]);
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20200410070422_first')
+BEGIN
+    CREATE INDEX [IX_AspNetUserLogins_UserId] ON [AspNetUserLogins] ([UserId]);
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20200410070422_first')
+BEGIN
+    CREATE INDEX [IX_AspNetUserRoles_RoleId] ON [AspNetUserRoles] ([RoleId]);
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20200410070422_first')
+BEGIN
+    CREATE INDEX [EmailIndex] ON [AspNetUsers] ([NormalizedEmail]);
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20200410070422_first')
+BEGIN
+    CREATE UNIQUE INDEX [UserNameIndex] ON [AspNetUsers] ([NormalizedUserName]) WHERE [NormalizedUserName] IS NOT NULL;
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20200410070422_first')
+BEGIN
+    CREATE INDEX [IX_Forums_GroupId] ON [Forums] ([GroupId]);
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20200410070422_first')
+BEGIN
+    CREATE INDEX [IX_PostReplies_PostId] ON [PostReplies] ([PostId]);
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20200410070422_first')
+BEGIN
+    CREATE INDEX [IX_PostReplies_UserId] ON [PostReplies] ([UserId]);
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20200410070422_first')
+BEGIN
+    CREATE INDEX [IX_Posts_ForumId] ON [Posts] ([ForumId]);
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20200410070422_first')
+BEGIN
+    CREATE INDEX [IX_Posts_UserId] ON [Posts] ([UserId]);
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20200410070422_first')
+BEGIN
+    CREATE INDEX [IX_PrivateMessages_ReceiverId] ON [PrivateMessages] ([ReceiverId]);
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20200410070422_first')
+BEGIN
+    CREATE INDEX [IX_PrivateMessages_SenderId] ON [PrivateMessages] ([SenderId]);
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20200410070422_first')
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20200410070422_first', N'2.0.0-rtm-26452');
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20200629115952_migration5')
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20200629115952_migration5', N'2.0.0-rtm-26452');
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20200629121316_migration6')
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20200629121316_migration6', N'2.0.0-rtm-26452');
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20200629122132_migration7')
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20200629122132_migration7', N'2.0.0-rtm-26452');
+END;
+
+GO
+
